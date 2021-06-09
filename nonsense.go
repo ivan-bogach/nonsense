1a720ce5c47d175b59541eaff6090dbac9f0442c
ivan-bogach <catmrgerman@gmail.com>
2021-06-09 11:10:23 +0300

    SendStringToTelegram

--- nonsense.go
+++ nonsense.go
@@ -1,6 +1,14 @@
 package nonsense
 
-// SliceContainsString reports whether slaice contains string.
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/user"
+	"strings"
+)
+
+// SliceContainsString reports whether slaice sl contains string s.
 func SliceContainsString(sl []string, st string) bool {
 	for _, s := range sl {
 		if s == st {
@@ -8,4 +16,27 @@ func SliceContainsString(sl []string, st string) bool {
 		}
 	}
 	return false
+}
+
+// SendStringToTelegram sends string s to a telegram bot with a token from telega.txt located in your root directory
+func SendStringToTelegram(s string) int {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := ioutil.ReadFile(usr.HomeDir + "/telega.txt")
+	if err != nil {
+		log.Fatal("you must create a file telega.txt with https://api.telegram.org/bottoken (e. g. https://api.telegram.org/bot110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw) in the root directory")
+		return 0
+	}
+	str := strings.TrimSuffix(string(b), "\n")
+	sendStr := str + s
+
+	resp, err := http.Get(sendStr)
+	if err != nil {
+		SendStringToTelegram("Again: " + s)
+	}
+
+	defer resp.Body.Close()
+	return resp.StatusCode
 }
--- nonsense_test.go
+++ nonsense_test.go
@@ -14,9 +14,30 @@ var SliceContainsStringTests = []struct {
 }
 
 func TestSliceContainsString(t *testing.T) {
+	t.Parallel()
+
 	for _, ct := range SliceContainsStringTests {
 		if SliceContainsString(ct.sl, ct.str) != ct.expected {
 			t.Errorf("SliceContainsString(%v, %s) = %v, want %v", ct.sl, ct.str, !ct.expected, ct.expected)
+		}
+	}
+}
+
+var SendStringToTelegramTests = []struct {
+	str      string
+	expected int
+}{
+	{"test", 200},
+	{"", 400},
+}
+
+func TestSendStringToTelegram(t *testing.T) {
+	t.Parallel()
+
+	for _, ct := range SendStringToTelegramTests {
+		sstt := SendStringToTelegram(ct.str)
+		if sstt != ct.expected {
+			t.Errorf("SendStringToTelegram(%s) = %d, want %v", ct.str, sstt, ct.expected)
 		}
 	}
 }
